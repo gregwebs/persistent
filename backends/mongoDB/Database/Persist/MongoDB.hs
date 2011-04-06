244e380f8ae9fa70389d281acbd368a6a5536d40
Rick Richardson <rick.richardson@gmail.com>
2011-04-06 16:40:18 -0400

    added MongoDB

---
+++ backends/mongoDB/Database/Persist/MongoDB.hs
@@ -0,0 +1,317 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE PackageImports, RankNTypes #-}
+-- | A redis backend for persistent.
+module Database.Persist.MongoDB
+    ( MongoDBReader
+    , withMongoDBConn
+    , runMongoDBConn 
+    , HostName
+    , PersistValue(..)
+    , keyToDbOid, dbOidToKey
+    , module Database.Persist
+    ) where
+
+import Database.Persist
+import Database.Persist.Base
+import Control.Monad.Trans.Reader
+import qualified Control.Monad.IO.Class as Trans
+import qualified Database.MongoDB as DB
+import Control.Applicative (Applicative)
+import Control.Exception (toException)
+import Data.UString (u)
+import qualified Data.CompactString.UTF8 as CS
+import Data.Enumerator hiding (map, length)
+import Network.Socket (HostName)
+import qualified Network.Abstract(NetworkIO)
+import Data.Maybe (catMaybes, fromJust)
+import qualified Data.Text as T
+import qualified Data.Text.Encoding as E
+import qualified Data.Serialize as S
+import qualified Data.ByteString as B
+
+
+newtype MongoDBReader t m a = MongoDBReader (ReaderT ((DB.ConnPool t), HostName) m a)
+    deriving (Monad, Trans.MonadIO, Functor, Applicative)
+
+withMongoDBConn :: (Network.Abstract.NetworkIO m) => t -> HostName -> ((DB.ConnPool DB.Host, t) -> m b) -> m b
+withMongoDBConn dbname hostname connectionReader = do
+  pool <- DB.newConnPool 1 $ DB.host hostname
+  connectionReader (pool, dbname)
+
+runMongoDBConn (MongoDBReader r) conn = do
+  runReaderT r conn
+
+runPool pool dbname action =
+  DB.access DB.safe DB.Master pool $ DB.use (DB.Database (u dbname)) action
+
+execute action = do
+  (pool, dbname) <- MongoDBReader ask
+  Right result <- runPool pool dbname action
+  return result
+
+value :: DB.Field -> DB.Value
+value (_ DB.:= val) = val
+
+rightPersistVals :: (PersistEntity val) => [DB.Value] -> (String -> String) -> val
+rightPersistVals vals err = case fromPersistValues (catMaybes (map DB.cast' vals)) of
+    Left e -> error (err e)
+    Right v -> v
+
+fst3 :: forall t t1 t2. (t, t1, t2) -> t
+fst3 (x, _, _) = x
+
+filterByKey :: (PersistEntity val) => Key val -> DB.Document
+filterByKey k = [u"_id" DB.=: fromPersistKey k]
+
+selectByKey :: forall val aQueryOrSelection.  (PersistEntity val, DB.Select aQueryOrSelection) => Key val -> EntityDef -> aQueryOrSelection
+selectByKey k entity = DB.select (filterByKey k) (u $ entityName entity)
+
+updateField :: (PersistEntity val) => [Update val] -> DB.Field
+updateField upds = u"$set" DB.=: (
+    zipWith (DB.:=) (updateLabels upds) (updateValues upds)
+  ) 
+  where
+  updateLabels = map (u . persistUpdateToFieldName)
+  updateValues = map (DB.val . persistUpdateToValue)
+
+uniqSelector :: forall val.  (PersistEntity val) => Unique val -> [DB.Field]
+uniqSelector uniq = zipWith (DB.:=)
+  (map u (persistUniqueToFieldNames uniq))
+  (map DB.val (persistUniqueToValues uniq))
+
+pairFromDocument :: forall val val1.  (PersistEntity val, PersistEntity val1) => [DB.Field] -> Either String (Key val, val1)
+pairFromDocument document = pairFromPersistValues $ catMaybes (map DB.cast' (map value document))
+  where
+    pairFromPersistValues (x:xs) =
+        case fromPersistValues xs of
+            Left e -> Left e
+            Right xs' -> Right (toPersistKey x, xs')
+    pairFromPersistValues _ = Left "error in fromPersistValues'"
+
+insertFields :: forall val.  (PersistEntity val) => EntityDef -> val -> [DB.Field]
+insertFields t record = zipWith (DB.:=) (toLabels) (toValues)
+  where
+    toLabels = map (u . fst3) $ entityColumns t
+    toValues = map (DB.val . toPersistValue) (toPersistFields record)
+
+instance (DB.DbAccess m, DB.Service t) => PersistBackend (MongoDBReader t m) where
+    insert record = do
+        (DB.ObjId key) <- execute $ DB.insert (u $ entityName t) (insertFields t record)
+        return $ toPersistKey $dbOidToKey key 
+      where
+        t = entityDef record
+
+    replace k record = do
+        execute $ DB.replace (selectByKey k t) (insertFields t record)
+        return ()
+      where
+        t = entityDef record
+
+    update _ [] = return ()
+    update k upds = do
+        execute $ DB.save (u $ entityName t)
+          [u"_id" DB.=: (fromPersistKey k), updateField upds] 
+      where
+        t = entityDef $ dummyFromKey k
+
+    updateWhere _ [] = return ()
+    updateWhere filts upds = do
+        execute $ DB.modify DB.Select {
+          DB.coll = (u $ entityName t)
+        , DB.selector = filterToSelector filts
+        } [updateField upds]
+      where
+        t = entityDef $ dummyFromFilts filts
+
+    delete k =
+        execute $ DB.deleteOne DB.Select {
+          DB.coll = (u $ entityName t)
+        , DB.selector = filterByKey k
+        }
+      where
+        t = entityDef $ dummyFromKey k
+
+    deleteWhere filts = do
+        execute $ DB.delete DB.Select {
+          DB.coll = (u $ entityName t)
+        , DB.selector = filterToSelector filts
+        }
+      where
+        t = entityDef $ dummyFromFilts filts
+
+    deleteBy uniq =
+        execute $ DB.delete DB.Select {
+          DB.coll = u $ entityName t
+        , DB.selector = uniqSelector uniq
+        }
+      where
+        t = entityDef $ dummyFromUnique uniq
+
+    get k = do
+            d <- execute $ DB.findOne (selectByKey k t)
+            case d of 
+              Nothing -> return Nothing
+              Just doc -> do 
+                let record = rightPersistVals (map value doc) (\e -> "get " ++ showPersistKey k ++ ": " ++ e)
+                return $ Just record
+          where
+            t = entityDef $ dummyFromKey k
+
+    getBy uniq = do
+        mdocument <- execute $ DB.findOne $
+          DB.select (uniqSelector uniq) (u $ entityName t)
+        case mdocument of
+          Nothing -> return Nothing
+          Just document -> case pairFromDocument document of
+              Left s -> error s
+              Right (k, x) -> return $ Just (k, x)
+      where
+        t = entityDef $ dummyFromUnique uniq
+
+    count filts = do
+        i <- execute $ DB.count query
+        return $ fromIntegral i
+      where
+        query = DB.select (filterToSelector filts) (u $ entityName t)
+        t = entityDef $ dummyFromFilts filts
+
+    selectEnum filts ords limit offset = do
+      Iteratee . start
+      where
+        start x = do
+            cursor <- execute $ DB.find query
+            loop x cursor
+
+        query = (DB.select (filterToSelector filts) (u $ entityName t)) {
+          DB.limit = fromIntegral limit
+        , DB.skip  = fromIntegral offset
+        , DB.sort  = if null ords then [] else map orderClause ords
+        }
+
+        t = entityDef $ dummyFromFilts filts
+        orderClause o = (u(persistOrderToFieldName o))
+                        DB.=: (case persistOrderToOrder o of
+                                Asc -> 1 :: Int
+                                Desc -> -1 )
+
+        loop (Continue k) curs = do
+            doc <- execute $ DB.next curs
+            case doc of
+                Nothing -> return $ Continue k
+                Just document -> case pairFromDocument document of
+                        Left s -> return $ Error $ toException
+                                $ PersistMarshalException s
+                        Right row -> do
+                            step <- runIteratee $ k $ Chunks [row]
+                            loop step curs
+        loop step _ = return step
+
+    selectKeys filts =
+        Iteratee . start
+      where
+        start x = do
+            cursor <- execute $ DB.find query
+            loop x cursor
+
+        loop (Continue k) curs = do
+            doc <- execute $ DB.next curs
+            case doc of
+                Nothing -> return $ Continue k
+                Just [_ DB.:= (DB.ObjId i)] -> do
+                    step <- runIteratee $ k $ Chunks [toPersistKey $ dbOidToKey i]
+                    loop step curs
+                Just y -> return $ Error $ toException $ PersistMarshalException
+                        $ "Unexpected in selectKeys: " ++ show y
+        loop step _ = return step
+
+        query = (DB.select (filterToSelector filts) (u $ entityName t)) {
+          DB.project = [u"_id" DB.=: (1 :: Int)]
+        }
+        t = entityDef $ dummyFromFilts filts
+
+filterToSelector :: PersistEntity val => [Filter val] -> DB.Document
+filterToSelector filts = map filterField filts
+
+filterField :: PersistEntity val => Filter val -> DB.Field
+filterField f = case filt of
+    Eq -> name DB.:= filterValue
+    _  -> name DB.=: [u(showFilter filt) DB.:= filterValue]
+  where
+    name = u $ persistFilterToFieldName f
+    filt = persistFilterToFilter f
+    filterValue = case persistFilterToValue f of
+      Left v -> DB.val v
+      Right vs -> DB.Array (map DB.val vs)
+
+    showFilter Ne = "$ne"
+    showFilter Gt = "$gt"
+    showFilter Lt = "$lt"
+    showFilter Ge = "$gte"
+    showFilter Le = "$lte"
+    showFilter In = "$in"
+    showFilter NotIn = "$nin"
+    showFilter Eq = error ""
+
+mapFromDoc :: DB.Document -> [(T.Text, PersistValue)]
+mapFromDoc = Prelude.map (\f -> ( ( csToT (DB.label f)), (fromJust . DB.cast') (DB.value f) ) )
+
+csToT :: CS.CompactString -> T.Text
+csToT = E.decodeUtf8 . CS.toByteString
+
+tToCS :: T.Text -> CS.CompactString
+tToCS = CS.fromByteString_ . E.encodeUtf8
+
+dbOidToKey :: DB.ObjectId -> PersistValue
+dbOidToKey =  PersistByteString . S.encode
+
+keyToDbOid :: B.ByteString -> DB.ObjectId
+keyToDbOid k = case S.decode k of
+                  Left s -> error s
+                  Right o -> o
+
+instance DB.Val PersistValue where
+  val (PersistInt64 x)   = DB.Int64 x
+  val (PersistText x)    = DB.String (tToCS x)
+  val (PersistDouble x)  = DB.Float x
+  val (PersistBool x)    = DB.Bool x
+  val (PersistUTCTime x) = DB.UTC x
+  val (PersistNull)      = DB.Null
+  val (PersistList l)    = DB.Array (map DB.val l)
+  val (PersistMap  m)    = DB.Doc $ Prelude.map (\(k, v)-> (DB.=:) (tToCS k) v) m
+  val (PersistByteString x) = DB.ObjId $ keyToDbOid x
+  cast' (DB.Float x) = Just (PersistDouble x)
+  cast' (DB.Int32 x)  = Just $ PersistInt64 $ fromIntegral x
+  cast' (DB.Int64 x)  = Just $ PersistInt64 x
+  cast' (DB.String x) = Just $ PersistText (csToT x) 
+  cast' (DB.Bool x)   = Just $ PersistBool x
+  cast' (DB.UTC d)    = Just $ PersistUTCTime d
+  cast' DB.Null       = Just $ PersistNull
+  cast' (DB.Bin (DB.Binary b))   = Just $ PersistByteString b
+  cast' (DB.Fun (DB.Function f)) = Just $ PersistByteString f
+  cast' (DB.Uuid (DB.UUID uid))  = Just $ PersistByteString uid
+  cast' (DB.Md5 (DB.MD5 md5))    = Just $ PersistByteString md5
+  cast' (DB.UserDef (DB.UserDefined bs)) = Just $ PersistByteString bs
+  cast' (DB.RegEx (DB.Regex us1 us2))    = Just $ PersistByteString $ CS.toByteString $ CS.append us1 us2
+  cast' (DB.Doc doc)  = Just $ PersistMap $ mapFromDoc doc
+  cast' (DB.Array xs) = Just $ PersistList $ catMaybes (map DB.cast' xs)
+  cast' _ = Nothing
+  -- val (PersistByteString bs) = DB.String $ CS.fromByteString bs
+  -- cast' (DB.ObjId (DB.Oid i32 i64)) = PersistInt64 $ fromIntegral i64
+  -- cast' (DB.JavaScr (DB.Javascript (Document doc) (us))) =
+  --val (PersistDay d) = H.SqlLocalDate d
+  --val (PersistTimeOfDay t) = H.SqlLocalTimeOfDay t
+
+instance S.Serialize DB.ObjectId where
+  put (DB.Oid w1 w2) = do S.put w1
+                          S.put w2
+
+  get = do w1 <- S.get
+           w2 <- S.get
+           return (DB.Oid w1 w2) 
+
+dummyFromKey :: Key v -> v
+dummyFromKey _ = error "dummyFromKey"
+dummyFromUnique :: Unique v -> v
+dummyFromUnique _ = error "dummyFromUnique"
+dummyFromFilts :: [Filter v] -> v
+dummyFromFilts _ = error "dummyFromFilts"
---
+++ backends/mongoDB/testdb.hs
@@ -0,0 +1,168 @@
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TypeSynonymInstances #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE TemplateHaskell #-}
+
+import Database.Persist.Quasi
+import Database.Persist.TH
+import Prelude hiding (filter)
+import Database.Persist
+import Database.Persist.MongoDB
+import qualified Network.Abstract(NetworkIO)
+import Control.Monad.Context
+import Control.Monad.IO.Class
+import qualified Data.Map as Map
+import Database.Persist.GenericSql
+import qualified Database.MongoDB as DB
+import Control.Monad.Throw
+
+mkPersist [$persist|
+Person 
+    name String update Eq Ne Desc In
+    age Int update "Asc" Lt "some ignored attribute"
+    color String null Eq Ne NotIn Ge
+    PersonNameKey name
+Pet
+    owner PersonId
+    name String
+Null
+    field Int null Eq Ne Gt NotIn In
+Table
+    table String
+|]
+
+main :: IO ()
+main = withMongoDBConn "test" "127.0.0.1" $ runMongoDBConn go
+
+go :: MongoDBReader DB.Host IO ()
+go = do
+    let x = DB.Oid 1234 56789
+    let s@(PersistByteString bs) = dbOidToKey x
+    let x' = keyToDbOid bs
+
+    liftIO $ print x
+    liftIO $ print s
+    liftIO $ print x' 
+      
+    deleteWhere ([] :: [Filter Person])
+
+    pid <- insert $ Person "Michael" 25 Nothing
+    liftIO $ print pid
+
+    p1 <- get pid
+    liftIO $ print p1
+
+    replace pid $ Person "Michael" 26 Nothing
+    p2 <- get pid
+    liftIO $ print p2
+
+    p3 <- selectList [PersonNameEq "Michael"] [] 0 0
+    liftIO $ print p3
+
+    _ <- insert $ Person "Michael2" 27 Nothing
+    deleteWhere [PersonNameEq "Michael2"]
+    p4 <- selectList [PersonAgeLt 28] [] 0 0
+    liftIO $ print p4
+
+    update pid [PersonAge 28]
+    p5 <- get pid
+    liftIO $ print p5
+
+    updateWhere [PersonNameEq "Michael"] [PersonAge 29]
+    p6 <- get pid
+    if fmap personAge p6 /= Just 29 then error "bug 57" else return ()
+    liftIO $ print p6
+
+    _ <- insert $ Person "Eliezer" 2 $ Just "blue"
+    p7 <- selectList [] [PersonAgeAsc] 0 0
+    liftIO $ print p7
+
+    _ <- insert $ Person "Abe" 30 $ Just "black"
+    p8 <- selectList [PersonAgeLt 30] [PersonNameDesc] 0 0
+    liftIO $ print p8
+
+    {-
+    insertR $ Person "Abe" 31 $ Just "brown"
+    p9 <- select [PersonNameEq "Abe"] []
+    liftIO $ print p9
+    -}
+
+    p10 <- getBy $ PersonNameKey "Michael"
+    liftIO $ print p10
+
+    p11 <- selectList [PersonColorEq $ Just "blue"] [] 0 0
+    liftIO $ print p11
+
+    p12 <- selectList [PersonColorEq Nothing] [] 0 0
+    liftIO $ print p12
+
+    p13 <- selectList [PersonColorNe Nothing] [] 0 0
+    liftIO $ print p13
+
+    p14 <- count [PersonColorNe Nothing]
+    liftIO $ print p14
+
+    delete pid
+    plast <- get pid
+    liftIO $ print plast
+
+    _ <- insert $ Person "Gavriella" 0 Nothing
+
+--    x@(_, Person "Gavriella" 0 Nothing) <-
+--        insertBy $ Person "Gavriella" 1 $ Just "blue"
+--    liftIO $ print x
+
+    False <- checkUnique $ Person "Gavriella" 2 Nothing
+    True <- checkUnique $ Person "Gavriela (it's misspelled)" 2 Nothing
+    return ()
+
+    p15 <- selectList [PersonNameIn $ words "Michael Gavriella"] [] 0 0
+    liftIO $ print p15
+
+    _ <- insert $ Person "Miriam" 23 $ Just "red"
+    p16 <- selectList [PersonColorNotIn [Nothing, Just "blue"]] [] 0 0
+    liftIO $ print p16
+
+    p17 <- selectList [PersonColorGe "blue"] [] 0 0
+    liftIO $ print p17
+
+    deleteWhere ([] :: [Filter Null])
+    _ <- insert $ Null $ Just 5
+    _ <- insert $ Null Nothing
+    [(_, Null (Just 5))] <- selectList [NullFieldGt 4] [] 0 0
+    [] <- selectList [NullFieldGt 5] [] 0 0
+    [(_, Null (Just 5))] <- selectList [NullFieldEq $ Just 5] [] 0 0
+    [(_, Null Nothing)] <- selectList [NullFieldNe $ Just 5] [] 0 0
+    [(_, Null Nothing)] <- selectList [NullFieldEq Nothing] [] 0 0
+    [(_, Null (Just 5))] <- selectList [NullFieldNe Nothing] [] 0 0
+    return ()
+
+    _ <- selectList ([] :: [Filter Person]) [] 0 0
+    _ <- selectList ([] :: [Filter Person]) [] 10 0
+    _ <- selectList ([] :: [Filter Person]) [] 10 10
+    _ <- selectList ([] :: [Filter Person]) [] 0 10
+
+    deleteWhere ([] :: [Filter Table])
+    _ <- insert $ Table "foo"
+    _ <- insert $ Table "bar"
+
+    return ()
+
+instance Context DB.MasterOrSlaveOk IO where
+  context = undefined
+  push = undefined
+instance Context DB.Database IO where
+  context = undefined
+  push = undefined
+instance Context DB.WriteMode IO where
+  context = undefined
+  push = undefined
+instance Context DB.Pipe IO where
+  context = undefined
+  push = undefined
+instance Control.Monad.Throw.Throw DB.Failure IO where
+  throw = undefined
+  catch = undefined
+
